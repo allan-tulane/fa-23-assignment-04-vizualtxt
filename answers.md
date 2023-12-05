b0e54c3569be920b69f0a94cf0f8359f98f031d9
vizualtxt <88392631+vizualtxt@users.noreply.github.com>
2023-12-05 00:04:38 +0000

    reloaded from replit

--- answers.md
+++ answers.md
@@ -1,8 +1,20 @@
 # CMPS 2200 Assignment 4
 ## Answers
 
-**Name:**_________________________
+**Name:** Ali Sulehria
 
 
 Place all written answers from `assignment-04.md` here for easier grading.
 
+1a) For N dollars, the greedy algorithm would be similar to partial-sums division: choose the greatest value coin that is less than N minus the sum, then add that value to the sum and repeat.
+
+b) Greedy choice property: the optimal solution is chosen by selecting the most optimal choice at every point (highest possible value without exceeding N)
+The optimal solution is found by picking the most optimal choice at every point, so the optimal substructure property holds.
+
+c) The work of this algorithm is W(n) = O(log n), as it is split with every step (at least in half), and it is not parallelized, so S = O(n) as well.
+
+2a) For a total N = 50, and denominations of 1, 2, 4, 5, 8, and 30. The greedy algorithm would choose 30 + 8 + 5 + 4 + 2 + 1, yielding 50 when the ideal solution would be 30 + 5 (x4) = 50 or 30 + (8 x 2) + 4.
+
+2b) This still has an optimal substructure property because at each point, i.e. for each subproblem, the algorithm is still taking into account the optimal solution and making its choice based on this. While the choice is no longer greedy, it is still optimal, leading to a globally optimal solution.
+
+2c) An algorithm to solve this prolem could use memoization to store the optimal solutions for the possible subproblems, ie the possible remaining sums. For each denomination value up to N, the optimal solution from that value to 0 is calculated and stored in a list. To find the global optimal solution, at each point one would choose the coin with the shortest path to 0.
--- main.py
+++ main.py
@@ -16,23 +16,57 @@ def MED(S, T):
         if (S[0] == T[0]):
             return(MED(S[1:], T[1:]))
         else:
-            return(1 + min(MED(S, T[1:]), MED(S[1:], T)))
+            return(1 + min(MED(S, T[1:]), MED(S[1:], T), MED(S[1:], T[1:])))
 
 
 def fast_MED(S, T, MED={}):
-    # TODO -  implement top-down memoization
-    pass
+    
+    if (S, T) in MED:
+      return MED[(S, T)]
+    if (S == ""):
+        return(len(T))
+    elif (T == ""):
+        return(len(S))
+    elif (S[0] == T[0]):
+      MED[(S, T)] = fast_MED(S[1:], T[1:], MED)
+      return MED[(S, T)]
+    else:
+      MED[(S, T)] = 1 + min(fast_MED(S, T[1:], MED), fast_MED(S[1:], T, MED))
+      return MED[(S, T)]
 
 def fast_align_MED(S, T, MED={}):
-    # TODO - keep track of alignment
-    pass
+    if (S, T) in MED:
+      return MED[(S,T)]
+    elif (S == ""):
+      MED[(S, T)] = (len(T), ("-"*len(T), T))
+    elif T == "":
+      MED[(S, T)] = (len(S), ("-"*len(S), S))
+    else:
+      if S[0] == T[0]:
+        MEDhold = fast_align_MED(S[1:], T[1:], MED)
+        MED[(S, T)] = (MEDhold[0], (S[0] + MEDhold[1][0], T[0] + MEDhold[1][1]))
+      else: 
+        insert = fast_align_MED(S, T[1:], MED)
+        delete = fast_align_MED(S[1:], T, MED)
+        #substitute = fast_align_MED(S[:-1], T, MED)
+        if insert[0] <= delete[0] and insert[0] <= substitute[0]:
+          MED[(S, T)] = (1 + insert[0], ('-' + insert[1][0], T[0] + insert[1][1]))
+        elif delete[0] <= insert[0] and delete[0] <= substitute[0]:
+          MED[(S, T)] = (1 + delete[0], (S[0] + delete[1][0], '-' + delete[1][1]))
+        #else: 
+        #  MED[(S, T)] = (substitute[0] + 1, (S[0] + substitute[1][0], T[0] + substitute[1][1]))
+    return MED[(S, T)]
+        
 
 def test_MED():
     for S, T in test_cases:
         assert fast_MED(S, T) == MED(S, T)
-                                 
+
+# there is some error in the alignment function, and it nearly matches the test cases.. unsure what to change but it's 99% there
 def test_align():
     for i in range(len(test_cases)):
         S, T = test_cases[i]
+        print(fast_align_MED(S, T))
         align_S, align_T = fast_align_MED(S, T)
         assert (align_S == alignments[i][0] and align_T == alignments[i][1])
+print(fast_align_MED("kookaburra", "kookybird"))
